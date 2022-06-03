5ac3d63f1aad1b85ea5fb66fa238a154be00d7cb
RichCode <stanleyogada2000@gmail.com>
2022-06-04 00:37:45 +0100

    chore:enabled wallaby.conf.js by renaming the file

--- wallaby.[conf].js
+++
@@ -1,5 +0,0 @@
-module.exports = function () {
-  return {
-    filesWithNoCoverageCalculated: ['src/**/*types.ts']
-  }
-}
---
+++ wallaby.conf.js
@@ -0,0 +1,6 @@
+module.exports = function () {
+  return {
+    // filesWithNoCoverageCalculated: ['src/**/*types.ts']
+    filesWithNoCoverageCalculated: ['src/components/types.ts', 'src/index.tsx']
+  }
+}
